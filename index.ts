1179f0ac479ae82c55a95bb5f1696265511b7e0b
Andrew Punnett <andrew.punnett@gmail.com>
2023-07-26 19:39:11 +1200

    Add a SomePartial utility type.

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-function greeter(person: string) {
-  return "Hello, " + person;
-}
+// Utility Types
 
-let user = "Jane User";
+type SomePartial<Type, Keys extends keyof Type> = Omit<Type, Keys> &
+  Partial<Pick<Type, Keys>>;
 
-console.log(greeter(user));
+type Point = { x: number; y: number; z: number };
+type SmallPoint = SomePartial<Point, "y" | "z">;
+
+const a: SmallPoint = { x: 1 };
+const b: SmallPoint = { x: 1, y: 2 };
+const c: SmallPoint = { x: 1, z: 3 };
+const d: SmallPoint = { x: 1, y: 2, z: 3 };
+// const e: SmallPoint = { y: 2 }; // ERROR: x is missing.
+// const e: SmallPoint = { x: 1, w: 4 }; // ERROR: w is not in the type.
